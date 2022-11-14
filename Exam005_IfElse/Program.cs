1acaa71aaf9bb969aaaa4afac4399535d37361e3
Andrey <hliyena@yandex.ru>
2022-11-14 23:00:02 +0500

    Exam 005 If Else

---
+++ Exam005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "андрей")
+{
+    Console.WriteLine("Ура! Это же Андрей!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
