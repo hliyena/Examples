46aa7fc65d411a8ddd9949428539524583fcc69d
Andrey <hliyena@yandex.ru>
2022-11-14 22:35:59 +0500

    Exam 004 Div

--- Exam004_Div/Program.cs
+++ Exam004_Div/Program.cs
@@ -1,4 +1,4 @@
 ﻿double numberA = new Random().Next(1,10); //1 2 3 4 ... 9
 double numberB = new Random().Next(1,10);
-int result = numberA + numberB;
+int result = (numberA + numberB);
 Console.WriteLine(result);
